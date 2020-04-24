ff19734b7a8a020b616537e51b5a7eaa48b941fb
Vitaly Lavrov <lve@guap.ru>
2020-04-24 13:07:35 +0300

    add scripts for importing records info from bbb-server

---
+++ db/import_info.php
@@ -0,0 +1,85 @@
+#!/usr/bin/env php72
+<?php
+
+define('CLI_SCRIPT', true);
+
+require(dirname(dirname(dirname(dirname(__FILE__)))).'/config.php');
+//require_once($CFG->libdir.'/clilib.php');      // cli only functions
+
+$strpar = array(
+	"id"=>"meetinguid",
+	"starttime"=>"starttime",
+	"total_time"=>"total_len",
+	"voice_time"=>"voice_len",
+	"rtc_time"=>"rtc_len",
+	"tcdesk_time"=>"tcdesk_len",
+	"pr_cnt"=>"pr_cnt",
+	"pr_pages"=>"pr_pages",
+	"audio"=>"audio_size",
+	"video"=>"video_size",
+	"deskshare"=>"deskshare_size");
+
+function get_eid_info($eid) {
+	
+$ret = array();
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
+curl_setopt($ch, CURLOPT_TIMEOUT, 2);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_COOKIEFILE, "");
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // On dev server only!
+curl_setopt($ch, CURLOPT_VERBOSE, false);
+
+curl_setopt($ch, CURLOPT_URL, 'https://bbb2.guap.ru/meeting_eid/'.urlencode($eid).'.txt');
+$result = curl_exec($ch);
+curl_close($ch);
+
+#print_r($result);
+
+foreach(explode("\n",$result) as $l) {
+	if(!strchr($l,":")) continue;
+	list($field, $data) = preg_split('/\s*:\s*/', $l);
+	if(!isset($strpar[$field])) continue;
+#	echo "$field : $data\n";
+	$ret[$strpar[$field]] = $data;
+}
+if(count($ret)) {
+	foreach($strpar as $k=>$v) {
+	  if(!isset($ret[$v])) $ret[$v] = 0;
+	}
+	$ret['server'] = 1;
+	$ret['meetingid'] = $eid;
+}
+return $ret;
+}
+
+$recs = $DB->get_records_sql("select bl.meetingid as eid,timecreated from {bigbluebuttonbn_logs} as bl
+	left join {bigbluebuttonbn_info} as bi on bl.meetingid = bi.meetingid
+	where bl.meetingid like '%-%' and bi.total_len is null");
+# print_r($recs);
+$ctm = time();
+foreach(array_keys($recs) as $eid ) {
+	$info = get_eid_info($eid);
+	if(count($info)) {
+		echo "ADD {$info['meetingid']}\n";
+		$DB->insert_record('bigbluebuttonbn_info',(object)$info,false,false);
+#		exit(0);
+	} else {
+		if($recs[$eid]->timecreated < $ctm - 24*3600) {
+			$info = array();
+			foreach($strpar as $k=>$v) { $info[$v] = 0; }
+			$info['server'] = 1;
+			$info['starttime'] = $recs[$eid]->timecreated;
+			$info['meetingid'] = $eid;
+			echo "BAD $eid\n";
+			$DB->insert_record('bigbluebuttonbn_info',(object)$info,false,false);
+		} else {
+			echo "WAIT $eid\n";
+		}
+#		exit(1);
+	}
+}
+
+exit(0); // 0 means success.
