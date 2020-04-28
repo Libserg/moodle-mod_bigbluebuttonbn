09dcaad98921ffa533cd28b0b107292c591f07bc
Vitaly Lavrov <lve@guap.ru>
2020-04-28 23:08:14 +0300

    Small fixes
    db/install.xml
    db/add_db_fields.php

---
+++ db/add_db_fields.php
@@ -0,0 +1,85 @@
+<?php
+
+define('CLI_SCRIPT', true);
+
+require(dirname(dirname(dirname(dirname(__FILE__)))).'/config.php');
+//require_once($CFG->libdir.'/clilib.php');      // cli only functions
+
+$CFG->debug=31;
+try {
+    if($DB->execute("select server from {bigbluebuttonbn} where server is not NULL"))
+	echo "bigbluebuttonbn server already added!\n";
+} catch (Exception $e) {
+#	echo 'Выброшено исключение: ',  $e->getMessage(), "\n";
+
+	if( $DB->execute("ALTER TABLE {bigbluebuttonbn} ADD COLUMN server bigint") && 
+	    $DB->execute("ALTER TABLE {bigbluebuttonbn} ALTER server SET DEFAULT 0") &&
+	    $DB->execute("UPDATE {bigbluebuttonbn} SET server=0 WHERE server is NULL")) {
+	    echo "bigbluebuttonbn server success added!\n";
+	}
+}
+try {
+    if($DB->execute("select uidlimit from {bigbluebuttonbn} where uidlimit is not NULL"))
+	echo "bigbluebuttonbn uidlimit already added!\n";
+} catch (Exception $e) {
+#	echo 'Выброшено исключение: ',  $e->getMessage(), "\n";
+
+	if( $DB->execute("ALTER TABLE {bigbluebuttonbn} ADD COLUMN uidlimit bigint") && 
+	    $DB->execute("ALTER TABLE {bigbluebuttonbn} ALTER uidlimit SET DEFAULT 0") &&
+	    $DB->execute("UPDATE {bigbluebuttonbn} SET uidlimit=0 WHERE uidlimit is NULL")) {
+	    echo "bigbluebuttonbn uidlimit success added!\n";
+	}
+}
+try {
+    if($DB->execute("select server from {bigbluebuttonbn_logs} where server is not NULL"))
+	echo "bigbluebuttonbn_logs server already added!\n";
+} catch (Exception $e) {
+#	echo 'Выброшено исключение: ',  $e->getMessage(), "\n";
+
+	if( $DB->execute("ALTER TABLE {bigbluebuttonbn_logs} ADD COLUMN server bigint") && 
+	    $DB->execute("ALTER TABLE {bigbluebuttonbn_logs} ALTER server SET DEFAULT 0") &&
+	    $DB->execute("UPDATE {bigbluebuttonbn_logs} SET server=0 WHERE server is NULL")) {
+	    echo "bigbluebuttonbn_logs server success added!\n";
+	}
+}
+
+try {
+    if($DB->execute("select norecinfo from {bigbluebuttonbn_logs} where norecinfo is not NULL"))
+	echo "bigbluebuttonbn_logs norecinfo already added!\n";
+} catch (Exception $e) {
+#	echo 'Выброшено исключение: ',  $e->getMessage(), "\n";
+
+	if( $DB->execute("ALTER TABLE {bigbluebuttonbn_logs} ADD COLUMN norecinfo int") && 
+	    $DB->execute("ALTER TABLE {bigbluebuttonbn_logs} ALTER norecinfo SET DEFAULT 0") &&
+	    $DB->execute("UPDATE {bigbluebuttonbn_logs} SET norecinfo=0 WHERE norecinfo is NULL")) {
+	    echo "bigbluebuttonbn_logs norecinfo success added!\n";
+	}
+}
+
+try {
+    if($DB->execute("select server from {bigbluebuttonbn_info} where server is not NULL"))
+	echo "bigbluebuttonbn_info already upgraded!\n";
+} catch (Exception $e) {
+	#	echo 'Выброшено исключение: ',  $e->getMessage(), "\n";
+	$sql = "CREATE TABLE {bigbluebuttonbn_info} (".
+		" id bigint PRIMARY KEY,".
+		" server smallint default 0,".
+		" meetingid varchar(128) not NULL,".
+		" meetinguid varchar(128) not NULL,".
+		" starttime bigint default 0,".
+		" users integer default 0,".
+		" total_len integer default 0,".
+		" voice_len integer default 0,".
+		" rtc_len integer default 0,".
+		" tcdesk_len integer default 0,".
+		" pr_cnt integer default 0,".
+		" pr_pages integer default 0,".
+		" audio_size integer default 0,".
+		" video_size integer default 0,".
+		" deskshare_size integer default 0)";
+	if( $DB->execute($sql)) {
+	    echo "bigbluebuttonbn_info success upgraded!\n";
+	}
+}
+
+exit(0); // 0 means success.
--- db/get_rec_info.php
+++ db/get_rec_info.php
@@ -32,6 +32,10 @@ require_once(__DIR__.'/../locallib.php');
 
 use mod_bigbluebuttonbn\plugin;
 
+$dbg = 0;
+$NO_REC = array();
+$EIDs = array();
+
 # help() {{{{
 function help($a='') {
     if($a) $a="Error: $a";
@@ -77,38 +81,50 @@ $strpar = array(
 
 function get_mid_info($mid) {
 global $strpar;	
-$ret = array();
+    $ret = array();
+    $result = '';
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
-curl_setopt($ch, CURLOPT_TIMEOUT, 2);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch, CURLOPT_COOKIEFILE, "");
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // On dev server only!
-curl_setopt($ch, CURLOPT_VERBOSE, false);
+    if(file_exists("rec_info/$mid"))
+        $result = file_get_contents("rec_info/$mid");
+    if(0) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 2);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, "");
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // On dev server only!
+        curl_setopt($ch, CURLOPT_VERBOSE, false);
 
-curl_setopt($ch, CURLOPT_URL, 'https://bbb2.guap.ru/meeting_id/'.urlencode($mid));
-$result = curl_exec($ch);
-curl_close($ch);
+        curl_setopt($ch, CURLOPT_URL, 'https://bbb2.guap.ru/meeting_id/'.urlencode($mid));
+        $result = curl_exec($ch);
+        curl_close($ch);
+    }
+    #print_r($result);
 
-#print_r($result);
-
-foreach(explode("\n",$result) as $l) {
-	if(!strchr($l,":")) continue;
-	list($field, $data) = preg_split('/\s*:\s*/', $l);
-	if(!array_key_exists($field,$strpar)) continue;
-#	echo "! '$field' {$strpar[$field]} : $data\n";
-	$ret[$strpar[$field]] = $data;
+    foreach(explode("\n",$result) as $l) {
+        if(!strchr($l,":")) continue;
+        list($field, $data) = preg_split('/\s*:\s*/', $l);
+        if(!array_key_exists($field,$strpar)) continue;
+    #	echo "! '$field' {$strpar[$field]} : $data\n";
+        $ret[$strpar[$field]] = $data;
+    }
+    if(count($ret)) {
+        foreach($strpar as $k=>$v) {
+          if(!isset($ret[$v])) $ret[$v] = 0;
+        }
+        $ret['server'] = 1;
+        $ret['meetingid'] = $eid;
+        $ret['starttime'] = intval($ret['starttime']);
+    }
+    return $ret;
 }
-if(count($ret)) {
-	foreach($strpar as $k=>$v) {
-	  if(!isset($ret[$v])) $ret[$v] = 0;
-	}
-	$ret['server'] = 1;
-	$ret['meetingid'] = $eid;
-}
-return $ret;
+
+function get_empty_info() {
+    global $strpar;
+    $ret = array();
+	foreach($strpar as $k=>$v) $ret[$v] = 0;
+    return $ret;
 }
 # }}}}
 
@@ -146,8 +162,8 @@ return $ret;
 
 list($options, $unrecognized) = cli_get_params(
         array('help' => false,
-                'verbose'=>false,
-                'id'=>false, 'idn'=>false,
+                'verbose'=>false,'sync'=>false,
+                'id'=>false, 'idn'=>false, 'check'=>false,
                 'list'=>false),
         array('h' => 'help', 'v'=>'verbose',
                 'l'=>'list')
@@ -165,23 +181,203 @@ if($options['list']) {
     echo "List\n";
     exit(0);
 }
+    $ar = file_get_contents('rec_info/record_all');
+    foreach(explode("\n",$ar) as $l) {
+        list($mid,$eid) = explode(" ",$l);
+        if(!$eid || !$mid) continue;
+        if(!isset($EIDs[$eid])) $EIDs[$eid] = [];
+        $EIDs[$eid][$mid] = [];
+    }
+    foreach(glob('rec_info/*') as $f1) {
+        if($f1 == 'rec_info/record_all') continue;
+        $info = explode("\n",file_get_contents($f1));
+        $id = false;
+        $eid = false;
+        $start = false;
+        $len = false;
+        foreach($info as $l) {
+            list($k,$v) = explode(":",$l);
+            if(!$k) continue;
+            $v = trim(rtrim($v));
+            if($k == 'id') $id = $v;
+            elseif($k == 'eid') $eid = $v;
+            elseif($k == 'starttime') $start = intval($v);
+            elseif($k == 'total_time') $len = intval($v);
+        }
+        if($id && $eid) {
+            if(!isset($EIDs[$eid])) $EIDs[$eid] = [];
+            if(!isset($EIDs[$eid][$id])) $EIDs[$eid][$id] = [];
+            $EIDs[$eid][$id] = [$start,$start+$len,$eid];
+        }
+    }
+#    print_r($EIDs);
+#    exit(0);
+if($options['check']) {
+    $logs_rec = $DB->get_records_sql(
+        "select id,meetingid,timecreated from {bigbluebuttonbn_logs} where log = ?",array('Create'));
+    foreach($logs_rec as $id => $r) {
+        if(!isset($EIDs[$r->meetingid])) {
+#            echo "UNK $id $r->meetingid\n";
+        } else {
+            $info = bigbluebuttonbn_get_recordings_array_cached($r->meetingid,1);
+            if(!$info) echo "BAD $id $r->meetingid\n";
+              else
+                echo "OK $id $r->meetingid\n";
+        }
+    }
+    
+    exit(0);
+}
+
+if($options['sync']) {
+    $logs_rec = $DB->get_records_sql("
+        select id,meetingid,timecreated from {bigbluebuttonbn_logs} where 
+        log = ? and norecinfo = 0 and id not in (select id from {bigbluebuttonbn_info})",array('Create'));
+    #print_r($logs_rec);
+    $n = 5000;
+    foreach($logs_rec as $rec) {
+        if(isset($NO_REC[$rec->meetingid])) continue;
+        if(!strchr($rec->meetingid,'-')) continue;
+        add_rec_info($rec->meetingid,0,intval($rec->timecreated));
+        $n--;
+        if($n < 0) break;
+    }
+    exit(0);
+}
+
 
 foreach($unrecognized as $rec) {
-    echo $rec,"\n";
-    add_rec_info($rec);
+    $dbg = 1;
+    add_rec_info($rec,1,0);
 }
 
 exit(0);
 
-function add_rec_info($rec) {
-    global $DB;
-#    $info = bigbluebuttonbn_get_recordings_array($rec,[],1);
-#    if(!$info) throw new \Exception("No info for $rec");
+function find_time_range(&$rec_tm_range,$ltc){
+    foreach($rec_tm_range as $id => $tr) {
+        $c = $ltc >= $tr[0] - 30  && $ltc <= $tr[1] + 30 ? 1:0;
+#        echo "  {$tr[0]} <= $ltc <= {$tr[1]} ",
+#            $ltc < $tr[0]-30 ? $ltc - $tr[0] : ($ltc > $tr[1]+30 ? $ltc - $tr[1]:0), "\n";
+        if($c) {
+#            echo "Found meetingid $id\n";
+            return $id;
+        }
+    }
+    return false;
+}
+function bigbluebuttonbn_get_recordings_array_cached($rec,$server=1) {
+    global $EIDs;
+#    if(file_exists("eid_info/$rec")) {
+#        return unserialize(file_get_contents("eid_info/$rec"));
+#    }
+    if(!isset($EIDs[$rec])) {
+        return false;
+    }
+    $info = array();
+    foreach($EIDs[$rec] as $id=>$v) {
+        $info[$id] = [];
+        $info[$id]['startTime'] = $v[0].'000';
+        $info[$id]['endTime'] = $v[1].'000';
+        $info[$id]['recordID'] = $id;
+    }
 #    print_r($info);
-    $blog_rec = $DB->get_records('bigbluebuttonbn_logs',array('meetingid'=>$rec,'log'=>'Create'),'','*');
-#            "select * from {bigbluebuttonbn_logs} where log = ?",
-#            array('Create'));
-    print_r(array($rec,$blog_rec));
+#    $info = bigbluebuttonbn_get_recordings_array($rec,[],1);
+#    if($info) {
+#        file_put_contents("eid_info/$rec",serialize($info));
+#    }
+    return $info;
+}
+
+function add_rec_info($rec,$dbg = false,$timecreated = 0) {
+    global $DB,$NO_REC;
+    $logs_rec = $DB->get_records('bigbluebuttonbn_logs',array('meetingid'=>$rec,'log'=>'Create'),'','*');
+    if(!count($logs_rec)) {
+        throw new \Exception("Bad log rec $rec");
+    }
+    $ilog_rec = $DB->get_records('bigbluebuttonbn_info',array('meetingid'=>$rec),'','*');
+    $info = bigbluebuttonbn_get_recordings_array_cached($rec,1);
+    if(!$info) {
+        if($timecreated && $timecreated < time() - 2*24*3600) {
+            foreach($logs_rec as $lid => $logs) {
+                if(isset($ilog_rec[$lid])) continue;
+                $DB->update_record('bigbluebuttonbn_logs',(object)array('id'=>$lid,'norecinfo'=>1));
+            }
+            echo "OLDREC $rec\n";
+        } else 
+            echo "NOREC $rec\n";
+
+        $NO_REC[$rec] = 1;
+        return;
+    }
+    echo $rec,' uid ',implode(',',array_keys($info)),"\n";
+    if($dbg) echo " info ",implode(',',array_keys($ilog_rec)),
+                    "\n log ",implode(',',array_keys($logs_rec)),"\n";
+
+    $rec_tm_range = array();
+    foreach($info as $imid => $m_info) {
+        $stime = intval(substr($m_info['startTime'],0,-3));
+        $etime = intval(substr($m_info['endTime'],0,-3));
+        $rec_tm_range[$imid] = [$stime,$etime];
+    }
+if($dbg) {
+    $srec_time = array();
+    foreach($logs_rec as $lid => $logs) {
+        $srec_time[] = intval($logs->timecreated);
+    }
+    sort($srec_time);
+    foreach($srec_time as $i => $ltc) {
+        $trid = find_time_range($rec_tm_range,$ltc);
+        if(!$trid) {
+            echo " $ltc -\n";
+        } else {
+            echo " $ltc $trid\n";
+        }
+    }
+}
+foreach($info as $imid => $m_info) {
+        if(!isset($m_info['recordID'])) continue;
+        $stime = intval(substr($m_info['startTime'],0,-3));
+        $etime = intval(substr($m_info['endTime'],0,-3));
+        $rec_info = get_mid_info($m_info['recordID']);
+        if(!$rec_info) {
+            echo "No info for {$m_info['recordID']}\n";
+            continue;
+        }
+        $rec_info['meetingid'] = $rec;
+
+#        echo "$imid start $stime end $etime\n";
+        $rec_info['starttime'] = $stime;
+        $rec_info['endtime'] = $etime;
+
+        foreach($logs_rec as $lid => $logs) {
+            if(isset($ilog_rec[$lid])) {
+                #echo "Skip lid $lid\n";
+                continue;
+            }
+            $ltc = intval($logs->timecreated);
+            $trid = find_time_range($rec_tm_range,$ltc);
+            if($trid != $imid) {
+                continue;
+            }
+            $rec_info['id'] = $lid;
+#            print_r(array($lid,$m_info['recordID'],$rec_info));
+            $DB->insert_record_raw('bigbluebuttonbn_info',$rec_info,false,false,true);
+            $ilog_rec[$lid] = 1;
+            echo "ADD $rec {$m_info['recordID']}\n";
+        }
+    }
+    if(count($ilog_rec) != count($logs_rec)) {
+        echo "part $rec\n";
+    }
+    foreach($logs_rec as $lid => $logs) {
+        if(isset($ilog_rec[$lid])) {
+            #echo "Skip lid $lid\n";
+            continue;
+        }
+        $ltc = intval($logs->timecreated);
+        if($ltc > time() - 2*24*3600) continue;
+        $DB->update_record('bigbluebuttonbn_logs',(object)array('id'=>$lid,'norecinfo'=>1));
+    }
 }
 
 
